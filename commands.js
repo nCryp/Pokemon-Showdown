f9b9db107d9694ba09341cfd9cef633e439b051b
ChampionOnyx <shisien_0928@live.com>
2013-11-03 22:17:41 +0800

    Update commands.js

--- commands.js
+++ commands.js
@@ -339,6 +339,13 @@ var commands = exports.commands = {
 		if (!user.joinRoom(targetRoom || room, connection)) {
 			return connection.sendTo(target, "|noinit|joinfailed|The room '"+target+"' could not be joined.");
 		}
+		if (target.toLowerCase() == "lobby") {
+					return connection.sendTo('lobby','|html|<div class="infobox" style="border-color:blue"><center><b><u>Welcome to The Sora League Server!</u></b></center><br /> ' +
+					'We are a Pokemon League open for challenges!<br /><br />' +
+					'You are welcomed to challenge us, participate in our tournaments or just chat in the lobby! <br /><br />' +
+					'The most important rule here is to have fun! We hope you will have an enjoyable stay here.<br /><br />' +
+					'You may want to check out <a href="http://soraleague.weebly.com/index.html">The Sora League Website</a> for details about the rules of challenge.</div>');
+		}
 	},
 
 	rb: 'roomban',
